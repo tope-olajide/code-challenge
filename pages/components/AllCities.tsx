f9b003416a57865df161c8ea7842aecf5043f276
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-10 12:22:54 +0100

    Create AllCities.tsx

---
+++ pages/components/AllCities.tsx
@@ -0,0 +1,16 @@
+import NavigationBar from "./NavigationBar"
+import Footer from "./Footer"
+
+const AllCities = () => {
+    return (
+        <>
+        <section className="main-section">
+        <NavigationBar />
+        <main className="">
+        </main>
+        <Footer />
+        </section>
+        </>
+    )
+}
+export default AllCities
