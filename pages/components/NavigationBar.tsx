547df055ef8d93aa110a8002b1c259b9cb0be803
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-10 12:22:37 +0100

    Create NavigationBar.tsx

---
+++ pages/components/NavigationBar.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+enum DataTheme {
+    ThemeOne = 'theme-1',
+    ThemeTwo = 'theme-2',
+    ThemeThree = 'theme-3',
+    ThemeFour = 'theme-4',
+    ThemeFive = 'theme-5',
+    ThemeSix = 'theme-6',
+  }
+const NavigationBar = () => {
+const [currentTheme, setCurrentTheme] = useState("")
+    const changeTheme = (themeColor:DataTheme) => {
+        document.documentElement.setAttribute('data-theme', themeColor);
+        setCurrentTheme(themeColor)
+        console.log(currentTheme);
+    }
+    return (
+        <>
+            <section className="main-header">
+                <section className="themes-container-left">
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeOne)}}></button>
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeTwo)}}></button>
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeThree)}}></button>
+                    
+                </section>
+                <h3>Weather App</h3>
+                <section className="themes-container-right">
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeFour)}}></button>
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeFive)}}></button>
+                    <button onClick={()=>{changeTheme(DataTheme.ThemeOne)}}></button>
+                    
+                </section>
+            </section>
+        </>
+    )
+}
+export default NavigationBar
