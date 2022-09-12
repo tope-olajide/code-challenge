669e08f1456e326c84fdabe565328b7be0223f89
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-12 13:36:19 +0100

    Update AddAndSearchCity.tsx

--- pages/components/AddAndSearchCity.tsx
+++ pages/components/AddAndSearchCity.tsx
@@ -1,31 +1,27 @@
-import { FormEvent } from "react";
 
-interface IAddAndSearchCityProps {
-    addCity(event: FormEvent): Promise<void>;
-    searchCity(event: FormEvent): void;
-    enteredCity: string;
-    setEnteredCity(city:string):void;
-}
+import { IAddAndSearchCityProps } from "../types";
+
 
 const AddAndSearchCity: React.FC<IAddAndSearchCityProps> = (props) => {
     return (
-      <>
-        <div>
-          <form className="add-and-search-city-form">
-            <input
-              type="text"
-              value={props.enteredCity}
-              onChange={(e) => props.setEnteredCity(e.target.value)}
-              className="add-city-input-field"
-            />
-            <div className="button-container">
-              <button className="add-city-button" onClick={(event) => props.addCity(event)}>Add City</button>
-              <button className="add-city-button" onClick={(event) => props.searchCity(event)}>Search City</button>
+        <>
+            <div>
+                <form className="add-and-search-city-form">
+                    <input
+                        type="text"
+                        value={props.enteredCity}
+                        onChange={(e) => props.setEnteredCity(e.target.value)}
+                        className="add-city-input-field"
+                    />
+                    <span className="add-city-error" style={props.isAddingCityError ? { display: "block" } : { display: "none" }}><p>⚠️The city could not be added. Please try again ⚠️. </p></span>
+                    <div className="button-container">
+                        <button className="add-city-button" onClick={(event) => props.addCity(event)}>{props.isAddingCity ? "Fetching City..." : "Add City"}</button>
+                        <button className="add-city-button" onClick={(event) => props.searchCity(event)}>Search City</button>
+                    </div>
+
+                </form>
             </div>
-            
-          </form>
-        </div>
-      </>
+        </>
     )
-  }
-  export default AddAndSearchCity
+}
+export default AddAndSearchCity
