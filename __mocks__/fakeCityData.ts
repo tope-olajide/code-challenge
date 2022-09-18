cec68ad7c94f74c186346b585da15aa7c3ec9e77
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-18 23:17:29 +0100

    Create fakeCityData.ts

---
+++ __mocks__/fakeCityData.ts
@@ -0,0 +1,13 @@
+export const fakeCityData = [{
+    name: 'Lagos',
+    temperature: 28,
+    country: 'NG',
+    weatherIconURL: 'https://openweathermap.org/img/wn/10n@4x.png',
+    description: 'light rain',
+    longitude: 3.75,
+    latitude: 6.5833,
+    fiveDaysForecastURL: "https://api.openweathermap.org/data/2.5/onecall?lat=6.5833&lon=3.75&exclude=current,hourly,minutely,alerts&units=metric&appid=4d8fb5b93d4af21d66a2948710284366",
+    feelsLike: 30.28,
+    humidity: 73,
+    pressure: 1015,
+}];
